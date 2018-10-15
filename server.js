7df47523f19c903567096bb7153d89644aca4dbf
JMJ90 <jakemjohnson90@gmail.com>
2018-10-15 12:08:53 -0500

    server config3

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000
 const app = express()
-//work
-module.exports = app
-
 if (process.env.NODE_ENV !== 'production') require('./secrets')
+module.exports = app
+//work
 
 app.use(express.static('public'));
+
+app.set('port', (process.env.PORT || 5000));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log("server running on ", PORT)
+app.listen(app.get('port'), () => {
+  console.log("server running on ", app.get('port'))
 })
