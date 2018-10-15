0e153c5dc0dfb238a276eb35abf0d3433971b07e
JMJ90 <jakemjohnson90@gmail.com>
2018-10-15 11:50:08 -0500

    sever

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000
 const app = express()
+//work
 module.exports = app
 
 if (process.env.NODE_ENV !== 'production') require('./secrets')
